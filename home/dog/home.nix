ee94c4f35b97d633e2c838cfdcce818f518766e1
dog <change_me@example.com>
2026-02-19 05:45:21 +0000

    First Build

---
+++ home/dog/home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "dog";
+  home.homeDirectory = "/home/dog";
+
+  # User Packages
+  home.packages = with pkgs; [
+    # Dev
+    vscode
+    ripgrep
+    jq
+    
+    # Term
+    htop
+    fastfetch
+    
+    # Web
+    firefox
+  ];
+
+  # Basic Git Config
+  programs.git = {
+    enable = true;
+    userName = "dog";
+    userEmail = "change_me@example.com";
+  };
+
+  # Shell Config
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      rebuild = "sudo nixos-rebuild switch --flake /home/dog/nix-config#nixos";
+    };
+  };
+
+  home.stateVersion = "24.05";
+}
