dba87d5654580f0ccdbf2f2a73eee9e4d2fcea45
endafk <254jaymz@gmail.com>
2026-02-20 03:04:07 +0300

    feat: Add GNOME desktop tweaks and remove unnecessary packages; update shell and program configurations

--- home/dog/programs.nix
+++ home/dog/programs.nix
@@ -6,9 +6,20 @@
     enable = true;
     userName = "endafk";
     userEmail = "254jaymz@gmail.com";
+    delta = {
+      enable = true;                     # pretty diffs with syntax highlighting
+      options = {
+        navigate = true;
+        side-by-side = true;
+        line-numbers = true;
+      };
+    };
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
+      merge.conflictstyle = "zdiff3";    # clearer conflict markers
+      diff.algorithm = "histogram";      # better diff output
+      rerere.enabled = true;             # remember conflict resolutions
     };
   };
 
@@ -37,11 +48,57 @@
         };
       }
     ];
+    interactiveShellInit = ''
+      # Disable greeting
+      set -g fish_greeting
+
+      # Better history search with up/down arrows
+      bind \e\[A history-search-backward
+      bind \e\[B history-search-forward
+    '';
+  };
+
+  # Tmux
+  programs.tmux = {
+    enable = true;
+    terminal = "tmux-256color";
+    mouse = true;
+    keyMode = "vi";
+    baseIndex = 1;                       # windows start at 1
+    escapeTime = 0;                      # no delay on Esc
+    historyLimit = 50000;
+    extraConfig = ''
+      # Split with | and -
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+
+      # Navigate panes with Alt+arrow (no prefix)
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
+
+      # Reload config
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded"
+
+      # Status bar
+      set -g status-position top
+      set -g status-style "bg=default,fg=white"
+    '';
   };
 
   # Direnv (auto-load .envrc per project)
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+
+  # fzf (fuzzy finder integration with shell)
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
+    defaultCommand = "rg --files --hidden --glob '!.git'";
+    defaultOptions = [ "--height=40%" "--layout=reverse" "--border" ];
   };
 }
